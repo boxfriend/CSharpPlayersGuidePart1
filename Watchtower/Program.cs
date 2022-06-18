748805ba26b2ea468a729d635fb6d08ab39add44
Boxfriend <boxfriend@users.noreply.github.com>
2022-06-18 09:07:31 -0400

    watchtower minor optimizations

--- Watchtower/Program.cs
+++ Watchtower/Program.cs
@@ -1,25 +1,21 @@
-﻿Console.WriteLine("Quick, which direction are they coming from?");
+Console.WriteLine("Quick, which direction are they coming from?");
 Console.Write("X: ");
 var x = Convert.ToInt32(Console.ReadLine());
 Console.Write("Y: ");
 var y = Convert.ToInt32(Console.ReadLine());
 
-if (x == 0 && y == 0)
+if (x + y == 0)
 {
     Console.WriteLine("Dear god, they're already on top of us!");
     return;
 }
 
-string xDir;
-if (x == 0)
-    xDir = "";
-else
+string xDir = "";
+if (x != 0)
     xDir = x > 0 ? "east" : "west";
 
-string yDir;
-if (y == 0)
-    yDir = "";
-else
+string yDir = "";
+if (y != 0)
     yDir = y > 0 ? "north" : "south";
 
 Console.WriteLine($"They are coming from {yDir}{xDir}");
